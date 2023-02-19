807a7687096894e5848d07a9c415fe175a7e118f
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-19 16:31:01 +0200

    Add buuton click test

--- client/src/components/Blog.test.js
+++ client/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders content', () => {
@@ -22,4 +23,27 @@ test('renders content', () => {
   // const { container } = render(<Blog blog={blog} />);
   // const div = container.querySelector('.blog');
   // expect(div).toHaveTextContent('maisy by m');
+});
+
+test('clicking the button calls the handler once', async () => {
+  const blog = {
+    title: 'maisy',
+    author: 'm',
+    likes: 0,
+    url: 'm',
+    user: {},
+  };
+
+  // a session is started to interact with the rendered component
+  const user = userEvent.setup();
+
+  const mockHandler = jest.fn();
+
+  render(<Blog blog={blog} />);
+
+  // The test finds the button based on the text from the rendered component and clicks the element:
+  const button = screen.getByText('View');
+  await user.click(button);
+
+  expect(button).toHaveTextContent('Hide');
 });
